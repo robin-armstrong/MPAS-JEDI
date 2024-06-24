1a9723726630213c6b105c07822b0e47c1d4eeae
Robin Armstrong <rja243@cornell.edu>
2024-06-24 11:19:19 -0600

    Added an application to run the quadrature update in MPAS-JEDI.

---
+++ src/mains/mpasQuadratureUpdate.cc
@@ -0,0 +1,30 @@
+/*
+ * (C) Copyright 2017 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include "oops/generic/instantiateModelFactory.h"
+#include "oops/runs/Run.h"
+#include "oops/runs/QuadratureUpdate.h"
+
+#include "saber/oops/instantiateCovarFactory.h"
+#include "saber/oops/instantiateLocalizationFactory.h"
+
+#include "ufo/instantiateObsErrorFactory.h"
+#include "ufo/instantiateObsFilterFactory.h"
+#include "ufo/ObsTraits.h"
+
+#include "mpasjedi/Traits.h"
+
+int main(int argc,  char ** argv) {
+  oops::Run run(argc, argv);
+  oops::instantiateModelFactory<mpas::Traits>();
+  saber::instantiateCovarFactory<mpas::Traits>();
+  saber::instantiateLocalizationFactory<mpas::Traits>();
+  ufo::instantiateObsErrorFactory();
+  ufo::instantiateObsFilterFactory();
+  oops::QuadratureUpdate<mpas::Traits, ufo::ObsTraits> var;
+  return run.execute(var);
+}
