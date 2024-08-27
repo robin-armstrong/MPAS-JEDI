68a5f20ac2e73e48d8bf87f720ff2a24f73771b0
Robin Armstrong <rja243@cornell.edu>
2024-08-27 14:39:45 -0600

    Shortened the executable name for the quadrature update application.

--- src/mains/mpasQuadratureUpdate.cc
+++ src/mains/mpasQuadratureUpdate.cc
@@ -1,21 +1,27 @@
 /*
- * (C) Copyright 2017 UCAR
+ * (C) Copyright 2024 UCAR
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-#include "oops/generic/instantiateModelFactory.h"
+/*
+ * (C) Copyright 2024 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
 #include "oops/runs/Run.h"
 #include "oops/runs/QuadratureUpdate.h"
 
+#include "oops/generic/instantiateModelFactory.h"
 #include "saber/oops/instantiateCovarFactory.h"
 #include "saber/oops/instantiateLocalizationFactory.h"
-
 #include "ufo/instantiateObsErrorFactory.h"
 #include "ufo/instantiateObsFilterFactory.h"
-#include "ufo/ObsTraits.h"
 
+#include "ufo/ObsTraits.h"
 #include "mpasjedi/Traits.h"
 
 int main(int argc,  char ** argv) {
@@ -25,6 +31,6 @@ int main(int argc,  char ** argv) {
   saber::instantiateLocalizationFactory<mpas::Traits>();
   ufo::instantiateObsErrorFactory();
   ufo::instantiateObsFilterFactory();
-  oops::QuadratureUpdate<mpas::Traits, ufo::ObsTraits> var;
-  return run.execute(var);
+  oops::QuadratureUpdate<mpas::Traits, ufo::ObsTraits> quadUpdate;
+  return run.execute(quadUpdate);
 }
